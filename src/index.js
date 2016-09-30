519350f56a696eb9151a1af0ac9b7aedbccca6c7
Andriy Chaika <andriy@chaika.in.ua>
2016-09-30 14:13:44 +0300

    Revert "PIXI quick loading"
    This reverts commit 6f2b55bb736d5f241cb0674ad6f9ac579e574430.
    
    # Conflicts:
    #	src/polyfill/index.js

--- src/index.js
+++ src/index.js
@@ -1,61 +1,28 @@
 // run the polyfills
-require('./polyfill/requestAnimationFrame');
+require('./polyfill');
 
-window.PIXI_loaded = false;
-window.onPixiReady = function (callback) {
-    function check() {
-        if (PIXI_loaded) {
-            return callback();
-        } else {
-            requestAnimationFrame(check);
-        }
-    }
+var core = module.exports = require('./core');
 
-    check();
-};
-
-requestAnimationFrame(function () {
-    requestAnimationFrame(function PIXI_polifill() {
-        require('./polyfill');
-        requestAnimationFrame(function PIXI_core() {
-            var core = null;
-            require('./core').init(function (result) {
-                core = result;
-
-                requestAnimationFrame(function PIXI_modules() {
-                    // add core plugins.
-                    //core.extras         = require('./extras');
-                    //core.filters        = require('./filters');
-                    //core.interaction    = require('./interaction');
-                    core.loaders = require('./loaders');
-                    //core.mesh           = require('./mesh');
-                    //core.accessibility  = require('./accessibility');
-
-                    requestAnimationFrame(function PIXI_loaders() {
+// add core plugins.
+//core.extras         = require('./extras');
+//core.filters        = require('./filters');
+//core.interaction    = require('./interaction');
+core.loaders        = require('./loaders');
+//core.mesh           = require('./mesh');
+//core.accessibility  = require('./accessibility');
 
 // export a premade loader instance
-                        /**
-                         * A premade instance of the loader that can be used to loader resources.
-                         *
-                         * @name loader
-                         * @memberof PIXI
-                         * @property {PIXI.loaders.Loader}
-                         */
-                        core.loader = new core.loaders.Loader();
+/**
+ * A premade instance of the loader that can be used to loader resources.
+ *
+ * @name loader
+ * @memberof PIXI
+ * @property {PIXI.loaders.Loader}
+ */
+core.loader = new core.loaders.Loader();
 
-                        // mixin the deprecation features.
-                        //Object.assign(core, require('./deprecation'));
+// mixin the deprecation features.
+//Object.assign(core, require('./deprecation'));
 
-                        // Always export pixi globally.
-                        global.PIXI = core;
-
-                        window.PIXI_loaded = true;
-
-                    });
-                });
-            });
-        });
-    });
-});
-
-
+// Always export pixi globally.
+global.PIXI = core;
--- src/polyfill/index.js
+++ src/polyfill/index.js
@@ -1,20 +1,4 @@
-function polyfill_ObjectAssign() {
-    require('./Object.assign');
-}
-
-//function polyfill_requestAnimationFrame() {
-//    require('./requestAnimationFrame');
-//}
-
-function polyfill_mathSign() {
-    require('./Math.sign');
-}
-
-polyfill_ObjectAssign();
-//polyfill_requestAnimationFrame();
-polyfill_mathSign();
+require('./Object.assign');
+require('./requestAnimationFrame');
+require('./Math.sign');
 require('./TypedArray');
-
-
-
-
